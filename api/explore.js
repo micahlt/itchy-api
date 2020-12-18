23731af886a003e3d0b97ab4d03dcf28a461b681
micahlt <micahincorporated@gmail.com>
2020-12-18 16:22:07 -0600

    🩹 Fix the explore endpoint

--- api/explore.js
+++ api/explore.js
@@ -2,12 +2,12 @@ var fetch = require('node-fetch');
 module.exports = (req, res) => {
   let supportedModes = ["popular", "trending", "recent"];
   let offset = req.query.offset || 0;
-  let tag = req.query.tag || "";
+  let tag = req.query.tag ? `&q=${req.query.tag}` : "";
   let mode = req.query.mode;
   if (!supportedModes.includes(mode)) {
     mode = "popular";
   }
-  fetch(`https://api.scratch.mit.edu/explore/projects?q=${tag}&offset=${offset}&limit=20&mode=${mode}`)
+  fetch(`https://api.scratch.mit.edu/explore/projects?&offset=${offset}&limit=20&mode=${mode}${tag}`)
     .then((response) => {
       if (response.ok) {
         return response.json();
