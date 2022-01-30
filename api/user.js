78b09a2c7274f6ad36c5ffc7509f585c62bb3e51
Micah Lindley <micahincorporated@gmail.com>
2022-01-29 21:04:27 -0600

    👤 Add followers field to user endpoint

--- api/user.js
+++ api/user.js
@@ -1,5 +1,8 @@
-var fetch = require('node-fetch');
-const html = require('node-html-parser');
+var fetch = require("node-fetch");
+const html = require("node-html-parser");
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(find, "g"), replace);
+}
 module.exports = (req, res) => {
   let offset = req.query.commentoffset || 0;
   let username = req.query.user || undefined;
@@ -11,63 +14,123 @@ module.exports = (req, res) => {
         if (response.ok) {
           return response.json();
         } else {
-          res.status(500).send(`There was an issue with the server: ${response.status}.`);
+          res
+            .status(500)
+            .send(`There was an issue with the server: ${response.status}.`);
         }
       })
       .then((data) => {
-        if (!returnComments) {
-          res.json(data);
-        } else {
-          // res.status(501).send(`Returning user comments has not been implemented.`);
-          fetch(`https://scratch.mit.edu/site-api/comments/user/${username}/?page=1`)
-            .then((response2) => {
-              if (response2.ok) {
-                return response2.text();
-              } else {
-                res.status(500).send(`There was an issue with the server: ${response2.status}.`);
-              }
-            })
-            .then((data2) => {
-              let c = html.parse(data2);
-              let commentArray = [];
-              c.querySelectorAll('li.top-level-reply').forEach((item, i) => {
-                if (!item.getAttribute('class').includes('removed')) {
-                  let id = item.querySelector('.comment').getAttribute('data-comment-id');
-                  let user = item.querySelector('#comment-user').getAttribute('data-comment-user');
-                  let content = item.querySelector('.content').innerText.replace(/\s+/g, ' ').trim();
-                  let timestamp = item.querySelector('.time').getAttribute('title');
-                  let replies = [];
-                  if (item.querySelector('.replies').innerHTML.trim() != 0) {
-                    item.querySelectorAll('li.reply').forEach((item2, i) => {
-                      let id2 = item2.querySelector('.comment').getAttribute('data-comment-id');
-                      let user2 = item2.querySelector('#comment-user').getAttribute('data-comment-user');
-                      let content2 = item2.querySelector('.content').innerText.replace(/\s+/g, ' ').trim();
-                      let timestamp2 = item2.querySelector('.time').getAttribute('title');
-                      replies.push({
-                        id: id2,
-                        user: user2,
-                        content: content2,
-                        timestamp: timestamp2
-                      });
-                    });
+        fetch(`https://scratch.mit.edu/users/${username}/followers`, {
+          headers: {
+            "User-Agent":
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
+          },
+        })
+          .then((res) => {
+            if (res.ok) {
+              return res.text();
+            } else {
+              res
+                .status(500)
+                .send(
+                  `There was an issue with the server: ${response.status}.`
+                );
+            }
+          })
+          .then((result) => {
+            const dom = html.parse(result);
+            let followers = dom.querySelector("h2");
+            followers = followers.text.split("(")[1].split(")")[0];
+            data.followers = parseInt(followers);
+            if (!returnComments) {
+              res.json(data);
+            } else {
+              fetch(
+                `https://scratch.mit.edu/site-api/comments/user/${username}/?page=1`
+              )
+                .then((response2) => {
+                  if (response2.ok) {
+                    return response2.text();
+                  } else {
+                    res
+                      .status(500)
+                      .send(
+                        `There was an issue with the server: ${response2.status}.`
+                      );
                   }
-                  commentArray.push({
-                    id,
-                    author: {
-                      username: user,
-                      image: 'https:' + item.querySelector('.avatar').getAttribute('src')
-                    },
-                    content,
-                    timestamp,
-                    replies
-                  });
-                }
-              })
-              res.json(commentArray);
-            });
-        }
-      })
+                })
+                .then((data2) => {
+                  let c = html.parse(data2);
+                  let commentArray = [];
+                  c.querySelectorAll("li.top-level-reply").forEach(
+                    (item, i) => {
+                      if (!item.getAttribute("class").includes("removed")) {
+                        let id = item
+                          .querySelector(".comment")
+                          .getAttribute("data-comment-id");
+                        let user = item
+                          .querySelector("#comment-user")
+                          .getAttribute("data-comment-user");
+                        let content = item
+                          .querySelector(".content")
+                          .innerText.replace(/\s+/g, " ")
+                          .trim();
+                        let timestamp = item
+                          .querySelector(".time")
+                          .getAttribute("title");
+                        let replies = [];
+                        if (
+                          item.querySelector(".replies").innerHTML.trim() != 0
+                        ) {
+                          item
+                            .querySelectorAll("li.reply")
+                            .forEach((item2, i) => {
+                              let id2 = item2
+                                .querySelector(".comment")
+                                .getAttribute("data-comment-id");
+                              let user2 = item2
+                                .querySelector("#comment-user")
+                                .getAttribute("data-comment-user");
+                              let content2 = item2
+                                .querySelector(".content")
+                                .innerText.replace(/\s+/g, " ")
+                                .trim();
+                              let timestamp2 = item2
+                                .querySelector(".time")
+                                .getAttribute("title");
+                              replies.push({
+                                id: id2,
+                                user: user2,
+                                content: content2,
+                                timestamp: timestamp2,
+                              });
+                            });
+                        }
+                        commentArray.push({
+                          id,
+                          author: {
+                            username: user,
+                            image:
+                              "https:" +
+                              item.querySelector(".avatar").getAttribute("src"),
+                          },
+                          content,
+                          timestamp,
+                          replies,
+                        });
+                      }
+                    }
+                  );
+                  res.json(commentArray);
+                });
+            }
+          });
+      });
   } else {
-    res.status(400).send(`The username query is required, but recieved a value of ${username}.`);
+    res
+      .status(400)
+      .send(
+        `The username query is required, but recieved a value of ${username}.`
+      );
   }
-}
+};
